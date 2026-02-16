8a969dbfaabbe30c4e6a1bedc46990e0db9c82d1
ABISH-01 <abisholeh801@gmail.com>
2026-02-16 13:03:20 +0700

    Add chatbot implementation in chatbot.py
    Implement chatbot functionality with NLTK and Keras model.

---
+++ chatbot.py
@@ -0,0 +1,58 @@
+import nltk
+from keras.models import load_model
+import pickle
+import numpy as np
+
+# Load model dan data
+model = load_model('chatbot_model.h5')
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+# Fungsi untuk membersihkan kalimat
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# Fungsi untuk membuat bag of words
+def bow(sentence, words, show_details=True):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print("ditemukan dalam bag: %s" % w)
+    return(np.array(bag))
+
+# Fungsi untuk memprediksi kelas
+def predict_class(sentence, model):
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+# Fungsi untuk mendapatkan respons
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+# Fungsi untuk memulai chatbot
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+# Contoh penggunaan
+print(chatbot_response("halo"))
